2ba542328e245419d95d1338e9867490e60a9ef3
Simon B <Mercury14@users.noreply.github.com>
2021-06-22 11:02:28 +0100

    reworked recursive function

---
+++ three_digit_palindrome.py
@@ -0,0 +1,28 @@
+# Project Euler - problem 4
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+# Find the largest palindrome made from the product of two 3-digit number
+
+# Work backwards froms highest possible values
+def problem_four():
+	for x in range(999, -1):
+		for y in range(999, -1):
+			number_to_check = x * y
+			if check_if_palindrome(number_to_check) == True:
+				print(number_to_check)
+				break
+			
+# use a recursive function to check if the next number is a palindrome
+def check_if_palindrome(number, start_index, end_index):
+	if (start_index == end_index):
+		return True
+		
+		if (number[start_index] != number[end]):
+			return False
+			
+		if (start_index < end_index+1):
+			return check_if_palindrome(number, start_index+1, end_index-1)
+			
+		return True
+
+# Test 
+problem_four()

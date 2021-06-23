a256ef18812bf493275ce18b9a6c575a4fd8108a
Simon B <bartlett.se@googlemail.com>
2021-06-23 17:00:57 +0100

    Update three_digit_palindrome

--- three_digit_palindrome.py
+++ three_digit_palindrome.py
@@ -2,27 +2,22 @@
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 # Find the largest palindrome made from the product of two 3-digit number
 
-# Work backwards froms highest possible values
-def problem_four():
-	for x in range(999, -1):
-		for y in range(999, -1):
-			number_to_check = x * y
-			if check_if_palindrome(number_to_check, 1, -1) == True:
-				print(number_to_check)
-				break
-
 # use a recursive function to check if the next number is a palindrome
-def check_if_palindrome(number, start_index, end_index):
-	if (start_index == end_index):
+def check_if_palindrome(number):
+	number =str(number)
+	if len(number) <=1:
 		return True
+	else:
+		return number[0] == number[-1] and check_if_palindrome(number[1:-1])
+	
 
-		if (number[start_index] != number[end]):
-			return False
+def is_pal():
+	highest = 1
+	for x in range(100, 1000):
+		for y in range(100, 1000):
+			if check_if_palindrome(x * y) and x * y > highest:
+				highest = x * y
+	return highest
 
-		if (start_index < end_index+1):
-			return check_if_palindrome(number, start_index+1, end_index-1)
+print(is_pal())
 
-		return True
-
-# Test
-print(problem_four())

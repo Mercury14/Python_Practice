95056b4b39dd1b7c9188b5ec15847fbe468e847a
Simon B <bartlett.se@googlemail.com>
2021-08-05 15:47:05 +0100

    Update to isIn function

--- Guttag_3rd_Ed/isIn_function.py
+++ Guttag_3rd_Ed/isIn_function.py
@@ -1,14 +1,18 @@
 # Write a function that accepts two strings as arguments and returns True if either string occurs anywhere in the other and False otherwise.
 
-# Capture first string
-string_1 = input("Enter the first string ")
-string_2 = input("Enter the second string ")
+def isIn():
+    # Capture first string
+    string_1 = input("Enter the first string ")
+    string_2 = input("Enter the second string ")
 
-if string_1 in string_2:
-    print("True")
+    if string_1 in string_2:
+        print("True")
 
-elif string_2 in string_1:
-    print("True")
+    elif string_2 in string_1:
+        print("True")
 
-else:
-    print("False")
+    else:
+        print("False")
+
+
+isIn()
